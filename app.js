ea672e29cf826a97e8fb63c53e9dcefbcdc72fe1
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-05-03 22:34:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,9 @@ app.use((req,res,next)=>{
 //     let registeredUser=await User.register(fakeUser,"hello world");
 //     res.send(registeredUser)
 // });
-
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"page not found"))
+})
 app.use("/listings",listingRouter)
 app.use("/listings/:id/reviews",reviewRouter)
 app.use("/",userRouter);
@@ -113,9 +115,7 @@ app.use("/",userRouter);
    
 
 // })
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"page not found"))
-})
+
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="something went wrong!"}=err;
     res.status(statusCode).render("error.ejs",{err});
