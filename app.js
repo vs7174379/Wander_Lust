4e69ddcbd2a9b805f8637fde146642bb96464055
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-05-03 22:24:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((req,res,next)=>{
 //     res.send(registeredUser)
 // });
 
-
+app.use("/listings",listingRouter)
 app.use("/listings/:id/reviews",reviewRouter)
 app.use("/",userRouter);
 
